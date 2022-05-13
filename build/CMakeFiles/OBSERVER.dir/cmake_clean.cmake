2495201d6bf9ed28d759a7b376d4fd1e1394767a
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-05-12 21:15:12 -0500

    xd 2

---
+++ build/CMakeFiles/OBSERVER.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OBSERVER.dir/src/main.cpp.obj"
+  "CMakeFiles/OBSERVER.dir/src/main.cpp.obj.d"
+  "OBSERVER.exe"
+  "OBSERVER.exe.manifest"
+  "OBSERVER.pdb"
+  "libOBSERVER.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OBSERVER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
