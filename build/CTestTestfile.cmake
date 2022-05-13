a163a257fb3e5ce0a0c30305f8c9813718a4d3b6
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-05-12 21:07:47 -0500

    xd

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mauri/source/repos/Software/design---observer-maidenlessn-t
-# Build directory: C:/Users/mauri/source/repos/Software/design---observer-maidenlessn-t/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
